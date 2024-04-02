8dedcf998e41c461e9176d8ce2da3ebf4719491e
Tân Thanh <Phamdinht70@gmail.com>
2024-04-02 16:28:30 +0700

    Add files via upload

---
+++ NgheNhacDemo/JavaSript/song.js
@@ -0,0 +1,26 @@
+let songs = [
+  {
+    name: "Blank Space",
+    path: "/Music/Taylor Swift - Blank Space.mp3",
+    artist: "Taylor Swift",
+    image: "/Images/BlankSpace.jpg",
+  },
+  {
+    name: "Cruel Summer",
+    path: "/Music/Taylor Swift - Cruel Summer (Official Audio).mp3",
+    artist: "Taylor Swift",
+    image: "/Images/CruelSummer.jpg",
+  },
+  {
+    name: "Shake It Off",
+    path: "/Music/Taylor Swift - Shake It Off.mp3",
+    artist: "Taylor Swift",
+    image: "/Images/ShakeItOff.jpg",
+  },
+  {
+    name: "You Belong With Me",
+    path: "/Music/Taylor Swift - You Belong With Me.mp3",
+    artist: "Taylor Swift",
+    image: "/Images/YouBelongWithMe.jpg",
+  },
+];
